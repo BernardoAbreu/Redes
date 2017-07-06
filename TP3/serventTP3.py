0eb0b0af4bcf5eaa4e8deb82d50db5397148bfa1
bernardoabreu <bernardoaabreu@gmail.com>
2017-07-06 01:41:41 -0300

    Add previous message check

--- TP3/serventTP3.py
+++ TP3/serventTP3.py
@@ -34,23 +34,108 @@ class Servent(object):
         self.sequence_number = 0
         self._handlers = (self.__handle_CLIREQ, self.__handle_QUERY)
 
+        self.received_msgs = set()
+
 
     def __read_input(self):
         pattern = re.compile(r'^\s*([^#\s][^\s]+)\s*([^\s].+[^\s])\s*$')
-        with open(self.input_file, 'r') as file:
+        with open(self.input_file, 'r') as f:
             return {line.group(1) : line.group(2)
-                    for line in map(pattern.match,file) if line is not None}
+                    for line in map(pattern.match, f) if line is not None}
 
 
-    def run(self):
+    def __decode_msg_type(self, msg_type):
+        msg_type = struct.unpack('!H',msg_type)[0]
 
-        while True:
-            data, addr = self.sock.recvfrom(MAX_SIZE) # buffer size is 1024 bytes
+        return msg_type
 
-            self.__handle_recv(data,addr)
+
+    def __encode_query(self, ttl, addr, sequence_number, text):
+        ip, port = addr
+        header1 = struct.pack('!HH', QUERY, ttl)
+        header2 = struct.pack('=4sl', socket.inet_aton(ip), socket.INADDR_ANY)
+        header3 = struct.pack('!HI', port, sequence_number)
+
+        return (header1 + header2 + header3 + text)
+
+
+    def __encode_response(self, key, value):
+        header = struct.pack('!H', RESPONSE)
+
+        return (header + key + '\t' + value + '\0')
+
+
+    def __decode_query_header(self, header):
+        ttl = struct.unpack('!H',header[:2])[0]
+        ip,ay  = struct.unpack('=4sl',header[2:10])
+        port,seq_number = struct.unpack('!HI', header[10:16])
+        ip =  socket.inet_ntoa(ip)
+        print ttl,ip,port,seq_number
+        return ttl,ip,port,seq_number
+
+
+    def __send_response(self, client_address, key, value):
+        response = self.__encode_response(key, value)
+        print 'response', response
+        # make header
+        self.sock.sendto(response, client_address)
+
+
+    def __check_for_key(self, client_address, key):
+
+        if key in self.values:
+            value = self.values[key]
+            print 'value', value
+            self.__send_response(client_address, key, value)
+
+
+    def __handle_CLIREQ(self, key, client_address):
+        print 'CLIREQ'
+
+        msg = self.__encode_query(TTL, client_address, 
+                self.sequence_number, key)
+
+        ip, port = client_address
+        self.received_msgs.add((ip, port, self.sequence_number, key))
+
+        self.sequence_number += 1
+
+        for neighbor in self.neighbors:
+            print neighbor
+            print msg
+            self.sock.sendto(msg, neighbor)
+        print 'here'
+        self.__check_for_key(client_address, key)
+
+
+    def __handle_QUERY(self, message_body, source_neighbor_addr):
+        print 'QUERY'
+
+        header = message_body[:(HEADER_SIZE-MSG_TYPE_SIZE)]
+        key = message_body[(HEADER_SIZE-MSG_TYPE_SIZE):]
+
+        ttl, ip, port, seq_number = self.__decode_query_header(header)
+
+        if (ip, port, seq_number, key) not in self.received_msgs:
+            self.received_msgs.add((ip, port, seq_number, key))
+            self.__check_for_key((ip, port), key)
+
+            ttl -= 1
+
+            if ttl > 0:
+                msg = self.__encode_query(ttl, (ip, port), seq_number, key)
+                for neighbor in self.neighbors:
+                    print neighbor,
+                    if neighbor != source_neighbor_addr:
+                        print ' ok',
+                        self.sock.sendto(msg, neighbor)
+                    print
+        else:
+            print "Already received"
 
 
     def __handle_recv(self, data, addr):
+
         print "received message:", data
         print 'from:',addr
 
@@ -64,93 +149,14 @@ class Servent(object):
             print >>sys.stderr, 'Unsupported message type'
 
 
-    # Message type (uint16_t) : 2 (QUERY),
-    # TTL (uint16_t),
-    # IP PORT (uint16_t) consulting client program
-    # Seq number (uint32_t)
-    # Text Key (Max 40 caracteres)
-    def __encode_query(self, ttl, addr, sequence_number, text):
-        ip, port = addr
-        header1 = struct.pack('!HH', QUERY, ttl)
-        header2 = struct.pack('=4sl', socket.inet_aton(ip), socket.INADDR_ANY)
-        header3 = struct.pack('!HI', port, sequence_number)
+    def run(self):
 
-        return (header1 + header2 + header3 + text)
+        while True:
+            data, addr = self.sock.recvfrom(MAX_SIZE)
 
-    def __encode_response(self, key, value):
-        header = struct.pack('!H', RESPONSE)
-
-        return (header + key + '\t' + value + '\0')
+            self.__handle_recv(data,addr)
 
 
-    def __decode_msg_type(self, msg_type):
-        msg_type = struct.unpack('!H',msg_type)[0]
-
-        return msg_type
-
-
-    def __decode_query_header(self, header):
-        ttl = struct.unpack('!H',header[:2])[0]
-        ip,ay  = struct.unpack('=4sl',header[2:10])
-        port,seq_number = struct.unpack('!HI', header[10:16])
-        ip =  socket.inet_ntoa(ip)
-        print ttl,ip,port,seq_number
-        return ttl,ip,port,seq_number
-
-
-
-    def __handle_CLIREQ(self, message_body, client_address):
-        print 'CLIREQ'
-        key_text = message_body
-        msg = self.__encode_query(TTL, client_address, 
-                self.sequence_number, key_text)
-
-        self.sequence_number += 1
-
-        for neighbor in self.neighbors:
-            print neighbor
-            print msg
-            self.sock.sendto(msg, neighbor)
-        print 'here'
-        self.__check_for_key(client_address, key_text)
-
-
-    def __handle_QUERY(self, message_body, source_neighbor_addr):
-        print 'QUERY'
-        print HEADER_SIZE
-        header = message_body[:(HEADER_SIZE-MSG_TYPE_SIZE)]
-        key = message_body[(HEADER_SIZE-MSG_TYPE_SIZE):]
-        print header
-        ttl, ip, port, seq_number = self.__decode_query_header(header)
-
-        print ttl, ip, port, seq_number
-        # check if message was received before
-
-        self.__check_for_key((ip, port), key)
-
-        # ttl -= 1
-
-        # if ttl > 0:
-        #     msg = __encode_query(ttl, (ip, port), seq_number, key_text)
-        #     for neighbor in self.neighbors:
-        #         if neighbor != source_neighbor_addr:
-        #             self.sock.sendto(msg, neighbor)
-
-
-        
-    def __check_for_key(self, client_address, key):
-
-        if key in self.values:
-            value = self.values[key]
-            print 'value', value
-            self.__send_response(client_address, key, value)
-
-
-    def __send_response(self, client_address, key, value):
-        response = self.__encode_response(key, value)
-        print 'response', response
-        # make header
-        self.sock.sendto(response, client_address)
 
 
 if __name__ == '__main__':
